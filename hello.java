6b2c76cbb441559c2862947a9803a63aa21c42f7
Paulo Roberto Rodrigues Soares <paulo.rodrigues@labs.dcx>
2023-06-01 11:46:00 -0300

    merge

--- hello.java
+++ hello.java
@@ -1,7 +1,9 @@
-public class Hello {
+public class hello {
   public static void main(String[] args) {
     System.out.println("Hello,Paulo !");
 
-    System.out.println("Hello World. Filipe !!")
+    System.out.println("Hello World. Filipe !!");
+
+    System.out.println("GIT merge");
   }
 }
